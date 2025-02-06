f78f21b6bf86e9b077548666b0a382765a6d0ee5
Денис Дугинов <duginov@grechka.digital>
2025-02-07 01:26:09 +0400

    test pre-commit

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
+
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.redis import RedisStorage
+
 from config import settings
 from src.handlers import register_handlers
 
@@ -16,11 +18,11 @@ dp = Dispatcher(storage=storage)
 register_handlers(dp)
 
 
-# Запуск бота
 async def main():
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     import asyncio
+
     asyncio.run(main())
